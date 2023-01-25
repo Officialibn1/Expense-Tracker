ade089e7adc38b9097afd0bc55197221a7cbac0d
Officialibn1 <officialibn001@gmail.com>
2023-01-26 00:03:07 +0100

    add the addTransaction function

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+
+import {GlobalContext} from '../context/GlobalState';
 const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
+    const {addTransaction} = useContext(GlobalContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 10000),
+            text,
+            amount: +amount
+        }
+
+        return addTransaction(newTransaction);
+    };
+
     return (
         <>
             <h3>Add New Transaction</h3>
-            <form action="submit">
+            <form action="submit" onSubmit={onSubmit} >
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" id="text" 
